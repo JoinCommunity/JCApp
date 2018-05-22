36731cc6d8f68bd28204f868e37a1f12badf7dfd
joselinoneto <joselinoneto@gmail.com>
2018-05-22 14:15:56 -0300

    new image assets, new cell

---
+++ JCApp/CustomViews/EventCustomCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  EventCustomCellTableViewCell.swift
+//  JCApp
+//
+//  Created by Jose Lino Neto on 22/05/18.
+//  Copyright © 2018 Jose Lino Neto. All rights reserved.
+//
+
+import UIKit
+
+class EventCustomCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
