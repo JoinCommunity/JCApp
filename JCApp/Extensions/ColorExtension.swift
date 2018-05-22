665d7b975b86a9baa7542f8a01ec4f3919226223
joselinoneto <joselinoneto@gmail.com>
2018-05-22 10:51:09 -0300

    colors

--- JCApp/Extensions/ColorExtension.swift
+++ JCApp/Extensions/ColorExtension.swift
@@ -6,4 +6,9 @@
 //  Copyright © 2018 Jose Lino Neto. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+extension UIColor {
+    public static let backGroundColor: UIColor = UIColor(red: CGFloat(0.0/255), green: CGFloat(137.0/255), blue: CGFloat(225.0/255), alpha: CGFloat(1.0))
+    public static let starYellow: UIColor = UIColor(red: CGFloat(1.0), green: CGFloat(0.835), blue: CGFloat(0), alpha: CGFloat(1.0))
+}
