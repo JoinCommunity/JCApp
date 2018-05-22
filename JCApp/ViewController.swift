12fb4b18aa2152cadc1e82b8e7635cfa265b9b2b
joselinoneto <joselinoneto@gmail.com>
2018-05-22 09:39:25 -0300

    Basic Project Structure

--- JCApp/ViewController.swift
+++ JCApp/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import JCApiClient
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let event = Event(title: "hello world")
+        
+        print(event.title)
     }
 
     override func didReceiveMemoryWarning() {
