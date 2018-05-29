81e59688a474b5660f6ef11b68f1c9a102b329cf
joselinoneto <joselinoneto@gmail.com>
2018-05-29 02:58:19 -0300

    Speaker details

--- JCApp/SpeakerDetailsViewController.swift
+++ JCApp/SpeakerDetailsViewController.swift
@@ -7,29 +7,29 @@
 //
 
 import UIKit
+import JCApiClient
 
 class SpeakerDetailsViewController: UIViewController {
 
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UITextView!
+    
+    var item: Event?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
+        guard let localEvent = self.item else {
+            return
+        }
+        
+        self.nameLabel.text = localEvent.name
+        self.descriptionLabel.text = localEvent.subject
+        self.profileImage.image = localEvent.image
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
