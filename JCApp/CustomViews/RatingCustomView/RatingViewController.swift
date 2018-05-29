26d23bf8df3a5557178690bae3791f67a2d12b45
joselinoneto <joselinoneto@gmail.com>
2018-05-29 11:47:43 -0300

    rating custom view

--- JCApp/CustomViews/RatingCustomView/RatingViewController.swift
+++ JCApp/CustomViews/RatingCustomView/RatingViewController.swift
@@ -7,13 +7,31 @@
 //
 
 import UIKit
+import Cosmos
 
 class RatingViewController: UIViewController {
 
+    @IBOutlet weak var commentTextField: UITextField!
+    @IBOutlet weak var ratingStarts: CosmosView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.ratingStarts.rating = 0
+        self.ratingStarts.settings.fillMode = .full
+        // Change the size of the stars
+        self.ratingStarts.settings.starSize = 40
+        self.ratingStarts.settings.starMargin = Double(self.view.frame.width / 15)
 
-        // Do any additional setup after loading the view.
+        self.ratingStarts.backgroundColor = UIColor.clear
+        
+        // Set the color of a filled star
+        self.ratingStarts.settings.filledColor = UIColor.starYellow
+        
+        // Set the border color of an empty star
+        self.ratingStarts.settings.emptyBorderColor = UIColor.starYellow
+        
+        // Set the border color of a filled star
+        self.ratingStarts.settings.filledBorderColor = UIColor.starYellow
     }
 
     override func didReceiveMemoryWarning() {
