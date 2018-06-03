37a673daf7eaaf6970e4e55c9cdb36bb8a9f4dd8
joselinoneto <joselinoneto@gmail.com>
2018-06-03 20:33:35 -0300

    new custom cell

---
+++ JCApp/CustomViews/SpeakerCustomCell/SpeakerCustomCellTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  SpeakerCustomCellTableViewCell.swift
+//  JCApp
+//
+//  Created by Jose Lino Neto on 03/06/18.
+//  Copyright © 2018 Jose Lino Neto. All rights reserved.
+//
+
+import UIKit
+
+class SpeakerCustomCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
